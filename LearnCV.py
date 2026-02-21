6b76c7e57dae64f8f22adb53a3592f38f3e37b10
Ishaan Gupta <ishaan7497@gmail.com>
2026-02-21 16:06:24 +0530

    Added thresholding to reduce noise

--- LearnCV.py
+++ LearnCV.py
@@ -11,10 +11,26 @@ while True:
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5,5), 0)
+
     diff = cv2.absdiff(prev_gray, gray)
 
+    ret, thresh = cv2.threshold(diff, 10, 255, cv2.THRESH_BINARY)
 
-    cv2.imshow("Diff", diff*3)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    count = 0
+    for i in contours:
+        if cv2.contourArea(i) > 800:
+                    x, y, w, h = cv2.boundingRect(i)
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    count += 1
+    print(count)
+
+    cv2.imshow("rect", frame)
+    cv2.imshow("Motion", thresh)
 
     prev_gray = gray
 
