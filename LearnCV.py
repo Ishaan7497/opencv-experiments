9ee4afe0ff09617d69161ca71eb9212d0460dedc
Ishaan Gupta <ishaan7497@gmail.com>
2026-02-21 15:41:22 +0530

    Implemented frame differencing for motion detection

--- LearnCV.py
+++ LearnCV.py
@@ -2,6 +2,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 ret, prev_frame = cap.read()
+prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
 
 while True:
     ret, frame = cap.read()
@@ -10,11 +11,12 @@ while True:
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    diff = cv2.absdiff(prev_gray, gray)
 
 
-    cv2.imshow("Camera", frame)
-    cv2.imshow("video",gray)
+    cv2.imshow("Diff", diff*3)
 
+    prev_gray = gray
 
     if cv2.waitKey(1) == ord('q'):
         break
