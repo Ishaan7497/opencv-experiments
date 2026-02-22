e8b9f549849dc72b9762f37fbf082ec2260966e9
Ishaan Gupta <ishaan7497@gmail.com>
2026-02-22 16:20:35 +0530

    Implemented HSV-based detection,contour filtering

--- LearnCV.py
+++ LearnCV.py
@@ -1,38 +1,39 @@
 import cv2
 cap = cv2.VideoCapture(0)
 
-ret, prev_frame = cap.read()
-prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+LOWER_BALL = (20, 60, 60) #testing with tennis ball
+UPPER_BALL = (50, 255, 255)
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         print("Failed")
         break
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv,LOWER_BALL,UPPER_BALL)
+    mask = cv2.erode(mask,None,iterations = 2)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5,5), 1)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if contours:
+        largest = max(contours, key = cv2.contourArea)
+        area = cv2.contourArea(largest)
 
-    diff = cv2.absdiff(prev_gray, gray)
+        if not (300 < area < 5000):
+            pass
+        else:
+            (x,y),radius = cv2.minEnclosingCircle(largest)
 
-    ret, thresh = cv2.threshold(diff, 10, 255, cv2.THRESH_BINARY)
+            if not (5 < radius < 600):
+                pass
+            else:
+                x,y,radius = int(x),int(y),int(radius)
+                cv2.circle(frame,(x,y),radius,(255,0),4)
 
-    thresh = cv2.dilate(thresh, None, iterations=2)
-
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    count = 0
-    largest = max(contours, key = cv2.contourArea)
-    if cv2.contourArea(largest) > 1000:
-        x, y, w, h = cv2.boundingRect(largest)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        count += 1
-    print(count)
 
     cv2.imshow("rect", frame)
-    cv2.imshow("Motion", thresh)
-
-    prev_gray = gray
+    cv2.imshow("Mask", mask)
 
     if cv2.waitKey(1) == ord('q'):
         break
